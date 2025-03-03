c3d58ad4e1ca31844725499356e26786db345829
Evan Gipson <evangipson@gmail.com>
2025-03-03 02:03:36 -0600

    Add docs to statement and lexemes

--- src/types/lexeme.h
+++ src/types/lexeme.h
@@ -1,13 +1,22 @@
 #ifndef LEXEME
 #define LEXEME
 
-typedef struct
+/// @struct lexeme
+/// @brief One or many words that will be compiled into a `statement`.
+typedef struct lexeme_struct
 {
+    /// @brief A collection of keywords, variables, or operators.
     char** tokens;
+    /// @brief A collection of expressions which aren't transformed before being invoked.
     char** expressions;
+    /// @brief The line number the `lexeme` was written on in the L# source code.
     unsigned int line_number;
 } lexeme;
 
+/// @brief Creates and returns a new `lexeme`.
+/// @param tokens A collection of keywords, variables, or operators.
+/// @param expressions A collection of expressions which aren't transformed before being invoked.
+/// @param line_number The line number the `lexeme` was written on in the L# source code.
 lexeme create_lexeme(char** tokens, char** expressions, unsigned int line_number);
 
 #endif
--- src/types/statement.h
+++ src/types/statement.h
@@ -4,16 +4,31 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-typedef struct
+/// @struct statement
+/// @brief Filled with compiler information and an `expression` that will be executed by the L# compiler.
+typedef struct statement_struct
 {
+    /// @brief The line number the `statement` started on in the L# source code.
     unsigned int line_number;
+    /// @brief The L# source file that this `statement` originates from.
     char* file_path;
+    /// @brief The type of a `statement`.
     char* type;
+    /// @brief The action of a `statement`.
     char* action;
+    /// @brief The name of a `statement`, for internal compiler use.
     char* name;
+    /// @brief The invokable behavior of a `statement`.
     bool (*expression)();
 } statement;
 
+/// @brief Creates and returns a new `statement`.
+/// @param line_number The line number the `statement` started on in the L# source code.
+/// @param file_path The L# source file that this `statement` originates from.
+/// @param type The type of the new `statement`.
+/// @param action The action of the new `statement`.
+/// @param name The name of the new `statement`.
+/// @param expression The `function` that defines the behavior of the new `statement`.
 statement create_statement(unsigned int line_number, char* file_path, char* type, char* action, char* name, bool (*expression)());
 
 #endif
